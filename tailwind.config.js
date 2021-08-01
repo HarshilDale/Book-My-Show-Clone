a8b129b601b53a247f105a2972d363638de35e4c
HarshilDale <harshil125.dale@gmail.com>
2021-08-01 15:34:32 +0530

    Done Successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
